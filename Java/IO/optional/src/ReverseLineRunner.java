a5fa0570a18dd40b71ea504d0c9f9c55d5f15046
Krbikau Kiryl <kebikov1995@gmail.com>
2020-06-24 15:12:21 +0300

    feat: add optional task #3

---
+++ Java/IO/optional/src/ReverseLineRunner.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+// 3. Read the text of the Java program and write the characters of each line in another file in the reverse order.
+public class ReverseLineRunner {
+    public static void main(String[] args) {
+        // Reading file 'Car.java', reverse reading line, writing in file 'CarReplacer.txt'.
+        System.out.println("Replace 'private' to 'public' in class Car:");
+        try(BufferedReader br = new BufferedReader(new FileReader("src\\Car.java"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("resources\\CarReverse.txt"))) {
+            String line;
+            while((line = br.readLine()) != null) {
+                StringBuilder sb = new StringBuilder(line);
+                String reverseLine  = sb.reverse().toString();
+                bw.write(reverseLine + '\n');
+                System.out.println(reverseLine);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
